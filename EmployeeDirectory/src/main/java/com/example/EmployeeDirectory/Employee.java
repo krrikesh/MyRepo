bce8f4b6eb4f44af87a0c72a8e2be7a87ddb08ba
krrikesh <48746570+krrikesh@users.noreply.github.com>
2019-03-20 14:49:03 +0530

    commit

---
+++ EmployeeDirectory/src/main/java/com/example/EmployeeDirectory/Employee.java
@@ -0,0 +1,34 @@
+package com.example.EmployeeDirectory;
+
+public class Employee {
+	
+	private int id;
+	private String name;
+	
+	public Employee(int id, String name) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
---
+++ EmployeeDirectory/src/main/java/com/example/EmployeeDirectory/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.example.EmployeeDirectory;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	
+	@RequestMapping(value="/employees", method=RequestMethod.GET, produces="application/json")
+	public List<Employee> getEmployee(){
+		
+		List<Employee> books = new LinkedList<>();
+		books.add(new Employee(1234, "Manish"));
+		books.add(new Employee(2345, "Vishal"));
+		return books;
+		
+	}
+
+}
